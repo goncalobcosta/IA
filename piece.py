21d0d70009f60eae974447a37eb5d11b7afb8d83
goncalobcosta <93865484+goncalobcosta@users.noreply.github.com>
2024-02-27 12:57:25 +0000

    fix bugz

--- piece.py
+++ piece.py
@@ -20,7 +20,7 @@ class Hydrogen(Piece):
         self.image = pygame.transform.smoothscale(pygame.image.load("atoms/hydrogen.png").convert_alpha(), (50, 50))
         
     def draw(self, surface):
-        surface.blit(self.image, (((800 - 9 * 50) // 2 + self.x*50, ((800 - 8 * 50) // 2 + self.y*50)) 
+        surface.blit(self.image, ((800 - 9 * 50) // 2 + self.x*50, ((800 - 8 * 50) // 2 + self.y*50)))
                
 class Oxygen(Piece):
     def __init__(self, x, y):
@@ -28,7 +28,7 @@ class Oxygen(Piece):
         self.image = pygame.transform.smoothscale(pygame.image.load("atoms/oxygen.png").convert_alpha(), (50, 50))
         
     def draw(self, surface):
-        pygame.draw.rect(self.image, self.color, ((800 - 9 * 50) // 2 + self.x*50, (800 - 8 * 50) // 2 + self.y*50, 46, 46))
+        surface.blit(self.image, ((800 - 9 * 50) // 2 + self.x*50, (800 - 8 * 50) // 2 + self.y*50, 46, 46))
      
 class Nitrogen(Piece):
     def __init__(self, x, y):
@@ -36,7 +36,7 @@ class Nitrogen(Piece):
         self.image = pygame.transform.smoothscale(pygame.image.load("atoms/nitrogen.png").convert_alpha(), (50, 50))
 
     def draw(self, surface):
-        pygame.draw.rect(self.image, self.color, ((800 - 9 * 50) // 2 + self.x*50, (800 - 8 * 50) // 2 + self.y*50, 46, 46))
+        surface.blit(self.image, ((800 - 9 * 50) // 2 + self.x*50, (800 - 8 * 50) // 2 + self.y*50, 46, 46))
      
 class Carbon(Piece):
     def __init__(self, x, y):
@@ -44,5 +44,5 @@ class Carbon(Piece):
         self.image = pygame.transform.smoothscale(pygame.image.load("atoms/carbon.png").convert_alpha(), (50, 50))
 
     def draw(self, surface):
-        pygame.draw.rect(self.image, self.color, ((800 - 9 * 50) // 2 + self.x*50, (800 - 8 * 50) // 2 + self.y*50, 46, 46))
+        surface.blit(self.image, ((800 - 9 * 50) // 2 + self.x*50, (800 - 8 * 50) // 2 + self.y*50, 46, 46))
     
